d865d42089bed71abef50784ac15feeb9cb96971
npartovi <npartovi@gmail.com>
2018-06-13 16:22:20 -0700

    profile-list-style 80% done

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import ProfileContainer from './profiles/profile_container';
+import Navbar from './navbar/navbar';
 
 export default class App extends Component {
   render() {
     return (
       <div className="profile-list-container">
+      	<Navbar />
       	<ProfileContainer />
       </div>
     );
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+class Navbar extends React.Component {
+	constructor(props){
+		super(props)
+	}
+
+	render(){
+		return(
+			<nav>
+				<img src="https://i.imgur.com/qgkP0co.png"></img>
+				
+			</nav>
+		)
+	}
+}
+
+export default Navbar

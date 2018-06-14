b062745c6b3348f50d59a1e0444f61282d32f9f4
npartovi <npartovi@gmail.com>
2018-06-13 17:00:01 -0700

    added navbar component

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -4,13 +4,28 @@ import React from 'react'
 class Navbar extends React.Component {
 	constructor(props){
 		super(props)
+
+		this.state = {
+			filter: "position"
+		}
+	}
+
+	renderFilterDropdown(){
+		return(
+			<div>
+				<select>
+					<option value="position">Position</option>
+					<option value="name">Name</option>
+				</select>
+			</div>
+		)
 	}
 
 	render(){
 		return(
 			<nav>
-				<img src="https://i.imgur.com/qgkP0co.png"></img>
-				
+				<img className="nav-bar-logo" src="https://i.imgur.com/qgkP0co.png"></img>
+				{this.renderFilterDropdown()}
 			</nav>
 		)
 	}
